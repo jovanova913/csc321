9e8c06e7af6b13c0ff0acf1eab9049abea965db0
ajovanovic1 <ajovanovic1@toromail.csudh.edu>
2025-04-09 01:02:24 +0000

    Lab 12 fixed

--- ajovanovic12.py
+++ ajovanovic12.py
@@ -7,20 +7,18 @@ def recArea(base, height):
 def recCircle(radius):
     return radius * radius * 3.14
 
-def main():
-    base = 0.0
-    height = 0.0
-    radius = 0.0
-    rectangle = 0.0
-    circle = 0.0
 
-    base = float(input("Enter a value for the base of a rectangle: "))
-    height = float(input("Enter a value for the height of the rectangle: "))
-    rectangle = recArea(base, height)
-    print("The area of your rectangle is: ", rectangle)
+base = 0.0
+height = 0.0
+radius = 0.0
+rectangle = 0.0
+circle = 0.0
 
-    radius = float(input("\nEnter a value for the radius of a circle: "))
-    circle = recCircle(radius)
-    print("The area of your circle is: ", circle)
+base = float(input("Enter a value for the base of a rectangle: "))
+height = float(input("Enter a value for the height of the rectangle: "))
+rectangle = recArea(base, height)
+print("The area of your rectangle is: ", rectangle)
 
-main()
+radius = float(input("\nEnter a value for the radius of a circle: "))
+circle = recCircle(radius)
+print("The area of your circle is: ", circle)
